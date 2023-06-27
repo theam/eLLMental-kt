8c734a0bcdc753e33c8fcb8ca1f0129057009a6f
Nick Tchayka <nick@booster.cloud>
2023-06-27 17:58:38 +0100

    Add more tests

--- llmental-core/src/test/kotlin/com/theagilemonkeys/llmental/core/handler/HandlerTest.kt
+++ llmental-core/src/test/kotlin/com/theagilemonkeys/llmental/core/handler/HandlerTest.kt
@@ -5,7 +5,7 @@ import io.kotest.matchers.shouldBe
 
 class HandlerTest : BehaviorSpec({
 
-    Given("a Handler") {
+    Given("a single Handler") {
         When("I initialize it with an input and output type") {
             Then("it should have a name") {
                 val sut = handler<String, Int> { s -> s.length }
@@ -13,10 +13,36 @@ class HandlerTest : BehaviorSpec({
             }
 
             Then("it should allow me to set a custom name") {
-                val sut2 = handler<String, Int>("MyCustomName"){ s ->
+                val sut2 = handler<String, Int>("MyCustomName") { s ->
                     s.length
                 }
                 sut2.name shouldBe "MyCustomName"
+            }
+        }
+
+        When("I initialize with some logic") {
+            Then("I can run it as if it was a function") {
+                val sut = handler<String, Int> { s -> s.length }
+                sut("Hello") shouldBe 5
+            }
+        }
+
+    }
+
+    Given("two handlers") {
+        When("one uses another") {
+            Then("they should appear in the dependency tree of the other") {
+                val handler1 = handler<String, Int> { s ->
+                    s.length
+                }
+                val handler2 = handler<Int, Int> { i ->
+                    val foo = handler1("Hello")
+                    foo + i
+                }
+                val result = handler2(5)
+                result shouldBe 10
+                handler2.uses shouldBe setOf(handler1)
+                handler1.usedBy shouldBe setOf(handler2)
             }
         }
     }
