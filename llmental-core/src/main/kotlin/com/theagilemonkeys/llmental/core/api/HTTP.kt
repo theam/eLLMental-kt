512f328e4fb217d9b82b9b46bacf1fc205fbed82
Nick Tchayka <nick@booster.cloud>
2023-06-29 18:02:46 +0100

    Document code

--- llmental-core/src/main/kotlin/com/theagilemonkeys/llmental/core/api/HTTP.kt
+++ llmental-core/src/main/kotlin/com/theagilemonkeys/llmental/core/api/HTTP.kt
@@ -5,12 +5,9 @@ package com.theagilemonkeys.llmental.core.api
 
 import com.theagilemonkeys.llmental.core.handler.Handler
 import kotlinx.coroutines.runBlocking
-import kotlinx.serialization.InternalSerializationApi
-import kotlinx.serialization.KSerializer
+import kotlinx.serialization.*
 import kotlinx.serialization.builtins.serializer
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.serializer
 import org.http4k.core.*
 import org.http4k.core.Status.Companion.OK
 import org.http4k.routing.RoutingHttpHandler
@@ -21,7 +18,22 @@ import org.http4k.server.KtorCIO
 import org.http4k.server.asServer
 import kotlin.reflect.KClass
 
+/**
+ * Converts an API to an HTTP app that is runnable with [Http4kServer.start].
+ *
+ * @param port the port to run the server on
+ */
 fun API.toHttpApp(port: Int = 9000): Http4kServer {
+
+    /**
+     * Runs a handler with some input and returns a response.
+     *
+     * @param input the input to run the handler with
+     * @param handler the handler to run
+     * @return 200 OK with the output of the handler if everything went well,
+     * 400 BAD REQUEST if the input could not be deserialized,
+     * 500 INTERNAL SERVER ERROR if the handler threw an exception.
+     */
     fun runHandler(input: Any, handler: Handler<Any, Any>): Response =
         try {
             // This will run the handler blocking ONLY IN THE CURRENT THREAD
@@ -32,10 +44,20 @@ fun API.toHttpApp(port: Int = 9000): Http4kServer {
             }
             val output = Json.encodeToString(handler.outputType.toSerializer(), result)
             Response(OK).body(output)
+        } catch (e: SerializationException) {
+            Response(Status.BAD_REQUEST).body(e.message ?: "Error serializing output")
         } catch (expectedException: Exception) {
             Response(Status.INTERNAL_SERVER_ERROR).body(expectedException.message ?: "Unknown error")
         }
 
+
+    /**
+     * Converts a handler to an HTTP endpoint that can be called with a POST request.
+     * The input of the handler is expected to be in the body of the request.
+     *
+     * @param handler the handler to convert
+     * @return an HTTP endpoint that can be called with a POST request
+     */
     fun toPost(handler: Handler<Any, Any>): RoutingHttpHandler =
         "/${handler.name}" bind Method.POST to { request: Request ->
             try {
@@ -48,6 +70,22 @@ fun API.toHttpApp(port: Int = 9000): Http4kServer {
             }
         }
 
+    /**
+     * Converts a handler to an HTTP endpoint that can be called with a GET request.
+     * The input of the handler is expected to be in the query parameters of the request.
+     * If the input type is a primitive type, the query parameter name is the name of the type.
+     *
+     * For example for a handler with input type Int, the query parameter name is "Int".
+     *
+     * And will be called like this:
+     *
+     * http://localhost:9000/greet?Int=1
+     *
+     * If the input type is a data class, the query parameters are the properties of the data class.
+     *
+     * @param handler the handler to convert
+     * @return an HTTP endpoint that can be called with a GET request
+     */
     fun toGet(handler: Handler<Any, Any>): RoutingHttpHandler =
         "/${handler.name}" bind Method.GET to { req: Request ->
             try {
@@ -66,12 +104,18 @@ fun API.toHttpApp(port: Int = 9000): Http4kServer {
         }
 
 
+    // Convert write operations to POST endpoints
     val writeRoutes = writeOperations.map { toPost(it.value) }
+
+    // Convert read operations to GET endpoints
     val readRoutes = readOperations.map { toGet(it.value) }
+
+    // Combine all endpoints
     val routesArray = writeRoutes + readRoutes
 
     val app = routes(list = routesArray.toTypedArray())
     return app.asServer(KtorCIO(port))
+
 }
 
 /**
