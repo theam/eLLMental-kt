d90be2cb014c84f08a9e4dc2b45c39c71a4a3ab8
Nick Tchayka <nick@booster.cloud>
2023-07-05 12:01:30 +0100

    Fix HTTP tests

--- llmental-core/src/test/kotlin/com/theagilemonkeys/llmental/core/api/HTTPKtTest.kt
+++ llmental-core/src/test/kotlin/com/theagilemonkeys/llmental/core/api/HTTPKtTest.kt
@@ -1,133 +1,138 @@
 package com.theagilemonkeys.llmental.core.api
 
-import com.theagilemonkeys.llmental.core.handler.handler
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import kotlinx.serialization.Serializable
 import org.http4k.client.JavaHttpClient
 import org.http4k.core.Method
 import org.http4k.core.Request
+import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 
 class HTTPKtTest : BehaviorSpec({
 
+    @Serializable
+    data class Input(val value: String)
+
+    /**
+     * Class under test
+     */
+    class MockService {
+        fun writeOp(name: String): String {
+            return "Hello $name"
+        }
+
+        fun readOp(name: String): String {
+            return "Goodbye $name"
+        }
+
+        fun writeOp2(input: Input): String {
+            return "Hello ${input.value}"
+        }
+
+        fun readOp2(input: Input): String {
+            return "Goodbye ${input.value}"
+        }
+
+        val api = MockService::class.apiDefinition {
+            write("writeOp") { input: String ->
+                writeOp(input)
+            }
+
+            read("readOp") { input: String ->
+                readOp(input)
+            }
+
+            write("writeOp2") { input: Input ->
+                writeOp2(input)
+            }
+
+            read("readOp2") { input: Input ->
+                readOp2(input)
+            }
+        }
+
+    }
+
+    /**
+     * Helper function to run a server perform a request, and assert on the response
+     */
+    fun withServiceCall(method: Method, route: String, body: String, block: (Response) -> Unit) {
+        val server = MockService().api.toHttpApp().start()
+
+        val client = JavaHttpClient()
+
+        val uri = "http://localhost:${server.port()}/MockService/${route}".replace(
+            "\"", "%22"
+        )
+
+        val request = Request(
+            method, uri
+        )
+            .body(body)
+
+        val response = client(request)
+
+        try {
+            block(response)
+        } finally {
+            server.stop()
+        }
+
+    }
+
+    /**
+     * Helper function over withServiceCall to run a write operation
+     */
+    fun withWriteOp(operation: String, body: String, block: (Response) -> Unit) {
+        withServiceCall(Method.POST, operation, body, block)
+    }
+
+    /**
+     * Helper function over withServiceCall to run a read operation
+     */
+    fun withReadOp(operation: String, body: String, block: (Response) -> Unit) {
+        withServiceCall(Method.GET, "$operation?$body", "", block)
+    }
+
+
+    /**
+     * The Actual Tests
+     */
     Given("an API converted to an HTTP app ") {
         When("it is called with primitive types") {
             Then("it should run write operations on POST") {
-
-                val greet = handler("greet") { input: Int ->
-                    "Hello ${input}"
+                withWriteOp("writeOp", "\"1\"") { response ->
+                    response.bodyString() shouldBe "\"Hello 1\""
+                    response.status shouldBe OK
                 }
-
-                val greetApi = api("GreetingService") {
-                    write(greet)
-                }
-
-                val server = greetApi.toHttpApp().start()
-
-                val client = JavaHttpClient()
-
-                val request = Request(
-                    Method.POST, "http://localhost:${server.port()}/greet"
-                )
-                    .body("1")
-
-                val response = client(request)
-
-                response.status shouldBe OK
-                response.bodyString() shouldBe "\"Hello 1\""
-
-                server.stop()
-
             }
 
             Then("it should run read operations on GET") {
-
-                val greet = handler("greet") { input: Int ->
-                    "Hello ${input}"
+                withReadOp("readOp", "String=\"1\"") { response ->
+                    response.bodyString() shouldBe "\"Goodbye 1\""
+                    response.status shouldBe OK
                 }
-
-                val greetApi = api("GreetingService") {
-                    read(greet)
-                }
-
-                val server = greetApi.toHttpApp().start()
-
-                val client = JavaHttpClient()
-
-                val request = Request(
-                    Method.GET, "http://localhost:${server.port()}/greet?Int=1"
-                )
-
-                val response = client(request)
-
-                response.status shouldBe OK
-                response.bodyString() shouldBe "\"Hello 1\""
-
-                server.stop()
             }
         }
+
         When("it is called with data classes") {
             Then("it should work with write operations") {
-
-                @Serializable
-                data class GreetInput(val name: String)
-
-                val greet = handler("greet") { input: GreetInput ->
-                    "Hello ${input.name}"
+                withWriteOp("writeOp2", "{\"value\":\"John\"}") { response ->
+                    response.bodyString() shouldBe "\"Hello John\""
+                    response.status shouldBe OK
                 }
-
-                val greetApi = api("GreetingService") {
-                    write(greet)
-                }
-
-                val server = greetApi.toHttpApp().start()
-
-                val client = JavaHttpClient()
-
-                val request = Request(
-                    Method.POST, "http://localhost:${server.port()}/greet"
-                )
-                    .body("{\"name\":\"John\"}")
-
-                val response = client(request)
-
-                response.status shouldBe OK
-                response.bodyString() shouldBe "\"Hello John\""
-
-                server.stop()
-
             }
 
             Then("it should work with read operations") {
-
-                @Serializable
-                data class GreetInput(val name: String)
-
-                val greet = handler("greet") { input: GreetInput ->
-                    "Hello ${input.name}"
+                withReadOp("readOp2", "value=John") { response ->
+                    response.bodyString() shouldBe "\"Goodbye John\""
+                    response.status shouldBe OK
                 }
-
-                val greetApi = api("GreetingService") {
-                    read(greet)
-                }
-
-                val server = greetApi.toHttpApp().start()
-
-                val client = JavaHttpClient()
-
-                val request = Request(
-                    Method.GET, "http://localhost:${server.port()}/greet?name=John"
-                )
-
-                val response = client(request)
-
-                response.status shouldBe OK
-                response.bodyString() shouldBe "\"Hello John\""
-
-                server.stop()
             }
         }
     }
 
+
 })
+
