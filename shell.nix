a07f79fda5ff50d0195caa01ae19a21970656438
Nick Tchayka <nick@booster.cloud>
2023-07-24 15:26:36 +0100

    Add development environment

---
+++ shell.nix
@@ -0,0 +1,21 @@
+# This file defines a development environment that will autoinstall all required
+# dependencies for developing eLLMental Kotlin properly. To use it:
+#
+# - Install Nix by following https://nixos.org/download.html
+# - Install direnv (https://direnv.net) either with Nix, brew, or your favorite way
+#
+# Add the following to your .zshrc:
+#
+# eval "$(direnv hook zsh)"
+# 
+# Run `direnv allow` in this directory to automatically get into the development shell 
+# whenever you `cd` into this directory.
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell rec {
+  buildInputs = with pkgs; [
+    gradle # Not really needed, but just in case
+    kotlin-language-server
+    jdk17
+    nodejs # For building the website
+  ];
+}
