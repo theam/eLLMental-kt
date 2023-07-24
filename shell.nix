66f97169b64b5f3192178858d39ffac77d878382
Nick Tchayka <nick@booster.cloud>
2023-07-24 16:24:47 +0100

    Improve dev-shell docs

--- shell.nix
+++ shell.nix
@@ -4,12 +4,20 @@
 # - Install Nix by following https://nixos.org/download.html
 # - Install direnv (https://direnv.net) either with Nix, brew, or your favorite way
 #
+# Put the following content to ~/.config/nixpkgs/config.nix.
+#
+# { allowUnfree = true; }
+#
 # Add the following to your .zshrc:
 #
 # eval "$(direnv hook zsh)"
 # 
 # Run `direnv allow` in this directory to automatically get into the development shell 
 # whenever you `cd` into this directory.
+#
+# To start programming run
+#
+# idea .
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell rec {
   buildInputs = with pkgs; [
@@ -17,5 +25,6 @@ pkgs.mkShell rec {
     kotlin-language-server
     jdk17
     nodejs # For building the website
+    jetbrains.idea-ultimate # IDE
   ];
 }
